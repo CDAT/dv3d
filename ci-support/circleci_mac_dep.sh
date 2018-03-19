89381bfb2e431cfdeb53d8cb761b27d3ed5ba671
Charles Doutriaux <doutriaux1@llnl.gov>
2018-03-19 13:18:10 -0700

    need to make sure proj4 is less than 5 (#18)
    * need to make sure proj4 is less than 5
    
    * need to install mesalib after the fact...
    
    * we need to install mesalib in a different step since vtk-cdat is on install line

--- ci-support/circleci_mac_dep.sh
+++ ci-support/circleci_mac_dep.sh
@@ -2,8 +2,10 @@
 ls
 pwd
 export PATH=${HOME}/miniconda/bin:${PATH}
-conda create -q -n py3 -c uvcdat/label/nightly -c nesii/channel/dev-esmf  -c conda-forge -c uvcdat "cdms2>2.12.2018.02.06" nose flake8 "python>3" cdat_info udunits2 mesalib nose flake8 "matplotlib<2.1" numpy=1.13 image-compare genutil vtk-cdat "cdutil>2.12.2017" cdtime nbsphinx easydev vcs
-conda create -q -n py2 -c uvcdat/label/nightly -c conda-forge -c uvcdat "cdms2>2.12.2018.02.06" cdat_info udunits2 nose flake8 mesalib nose image-compare flake8 "matplotlib<2.1" numpy=1.13 genutil vtk-cdat "cdutil>2.12.2017" cdtime nbsphinx easydev vcs
+conda create -q -n py2 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdms2 cdat_info udunits2 nose flake8 image-compare matplotlib numpy=1.13 genutil vtk-cdat cdutil vcs "proj4<5" "python<3"
+conda install -q -n py2 -c cdat/label/nightly -c conda-forge mesalib
+conda create -q -n py3 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdms2 cdat_info udunits2 nose flake8 image-compare matplotlib numpy=1.13 genutil vtk-cdat cdutil vcs "proj4<5" "python>3"
+conda install -q -n py3 -c cdat/label/nightly -c conda-forge mesalib
 export UVCDAT_ANONYMOUS_LOG=False
 source activate py2
 python setup.py install
