cc7503285a5e09165c7eea12599dfc5e002cd8cb
Charles Doutriaux <doutriaux1@llnl.gov>
2018-10-24 07:23:06 -0700

    fix logo, upload only once

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
 from setuptools import setup, find_packages
 import os, sys
 import glob
+from subprocess import PIPE, Popen
+Version = "8.0"
+p = Popen(
+    ("git",
+     "describe",
+     "--tags"),
+    stdin=PIPE,
+    stdout=PIPE,
+    stderr=PIPE)
+try:
+    descr = p.stdout.readlines()[0].strip().decode("utf-8")
+    Version = "-".join(descr.split("-")[:-2])
+    if Version == "":
+        Version = descr
+except:
+    descr = Version
 
+ 
 buttons = glob.glob("data/buttons/*")
 coast_low = glob.glob("data/coastline/low/*")
 coast_medium = glob.glob("data/coastline/medium/*")
@@ -9,11 +26,11 @@ coast_high = glob.glob("data/coastline/high/*")
 package_data = ['data/earth2k.jpg', 'data/colormaps.pkl', 'data/parameters.txt']
 packages = find_packages()
 
-print(coast_low)
-setup (name = "DV3D",
-       description = "Climate Data Interactive Visualization System",
-       url = "http://portal.nccs.nasa.gov/DV3D/",
-       packages = packages,
+setup (name="DV3D",
+       version=descr,
+       description="Climate Data Interactive Visualization System",
+       url="http://portal.nccs.nasa.gov/DV3D/",
+       packages=packages,
        data_files=[('share/dv3d', package_data),
                    ('share/dv3d/buttons', buttons),
                    ('share/dv3d/coastline', ('data/coastline/index.txt',)),
@@ -22,4 +39,3 @@ setup (name = "DV3D",
                    ('share/dv3d/coastline/high', coast_high),
                    ]
        )
-
